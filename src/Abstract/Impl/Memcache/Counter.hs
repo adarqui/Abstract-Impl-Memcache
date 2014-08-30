d0af68d98cab6360b9dc6c5f3d113684b675e705
Andrew Darqui <andrew.darqui@gmail.com>
2014-08-30 13:11:38 -0400

    forgot defaults

--- src/Abstract/Impl/Memcache/Counter.hs
+++ src/Abstract/Impl/Memcache/Counter.hs
@@ -1,5 +1,7 @@
 module Abstract.Impl.Memcache.Counter (
  module Abstract.Interfaces.Counter,
+ counterMemcacheWrapper'Int,
+ defaultCounterMemcacheWrapper'Int,
  mkCounter'Memcache'Int,
  mkCounter'Memcache'Int'Inc,
  mkCounter'Memcache'Int'Dec,
@@ -7,7 +9,8 @@ module Abstract.Impl.Memcache.Counter (
 ) where
 
 import Abstract.Interfaces.Counter
-import Abstract.Impl.Memcache.Counter.Internal (mkCounter'Memcache'Int)
+import Abstract.Impl.Memcache.Counter.Internal
+ (mkCounter'Memcache'Int, counterMemcacheWrapper'Int, defaultCounterMemcacheWrapper'Int)
 import Abstract.Impl.Memcache.Counter.Inc (mkCounter'Memcache'Int'Inc)
 import Abstract.Impl.Memcache.Counter.Dec (mkCounter'Memcache'Int'Dec)
 import Abstract.Impl.Memcache.Counter.Get (mkCounter'Memcache'Int'Get)
